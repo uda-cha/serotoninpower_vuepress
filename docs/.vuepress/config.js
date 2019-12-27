b190e1f5642f3b80ac3ee4e73368983e55be4154
uda-cha <mine_s@zd6.so-net.ne.jp>
2019-12-27 22:24:08 +0900

    add vuepress-plugin-autometa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,12 +9,12 @@ module.exports = {
   },
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'og:url', content: 'https://www.serotoninpower.club/' }],
-    ['meta', { name: 'og:type', content: 'website' }],
-    ['meta', { name: 'og:title', content: 'せろとにんぱわー.' }],
-    ['meta', { name: 'og:description', content: 'インフラやミドルウェア周りの話が多め。' }],
+    //['meta', { name: 'og:url', content: 'https://www.serotoninpower.club/' }],
+    //['meta', { name: 'og:type', content: 'website' }],
+    //['meta', { name: 'og:title', content: 'せろとにんぱわー.' }],
+    //['meta', { name: 'og:description', content: 'インフラやミドルウェア周りの話が多め。' }],
     ['meta', { name: 'og:image', content: 'https://www.serotoninpower.club/og.jpg' }],
-    ['meta', { name: 'twitter:card', content: 'summary' }],
+    //['meta', { name: 'twitter:card', content: 'summary' }],
     ['meta', { name: 'twitter:site', content: 'https://www.serotoninpower.club/' }]
   ],
   globalUIComponents: [
@@ -45,6 +45,17 @@ module.exports = {
        canonical_base: 'https://www.serotoninpower.club/',
        posts_directories: ['/archives/'],
        sort: entries  => entries.sort((a, b) => b.frontmatter.date - a.frontmatter.date)
+    },
+    'autometa': {
+      site: {
+        name   : 'せろとにんぱわー.',
+      },
+      image: false,
+      image_sources: [
+        'frontmatter',
+        /<img.*?src=['"](.*?)['"]/i,
+      ],
+      canonical_base: 'https://www.serotoninpower.club/',
     }
   }
 }

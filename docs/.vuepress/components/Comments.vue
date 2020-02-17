77e9cfc80a34d4ea07c36c4d23046364e35e088d
uda-cha <mine_s@zd6.so-net.ne.jp>
2020-02-17 23:50:22 +0900

    add Comments.vue and comment sandbox

---
+++ docs/.vuepress/components/Comments.vue
@@ -0,0 +1,25 @@
+// Comments.vue
+<template>
+  <iframe
+    id="comentFrame"
+    v-bind:src="get_ifram_src_url"
+    frameborder="0"
+    height="800px"
+    width="100%"
+    >
+    コメントを表示するには iframe 機能を有効にして下さい
+  </iframe>
+</template>
+<script>
+export default {
+  computed: {
+    get_ifram_src_url: function() {
+      const url =
+        'https://serotoninpower-cms.herokuapp.com/posts/' +
+        this.$page.regularPath.split('/').slice(-2)[0] +
+        '/comments/wrapper';
+      return url;
+    },
+  },
+};
+</script>
---
+++ docs/archives/sandbox/index.md
@@ -0,0 +1,18 @@
+---
+title: Comment Sandbox
+description: "コメントを試す用のページ"
+author: seroto_nin
+sidebar: false
+feed:
+  enable: false
+sitemap:
+  exclude: true
+---
+
+# {{this.$page.frontmatter.title}}
+
+{{this.$page.frontmatter.description}}
+
+<!--more-->
+
+<Comments />

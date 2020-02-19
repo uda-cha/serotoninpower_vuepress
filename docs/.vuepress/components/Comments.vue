ab22b29e91d14075e69777cc4b207284da833d46
uda-cha <mine_s@zd6.so-net.ne.jp>
2020-02-20 00:32:45 +0900

    Provisional support for issues that cannot be commented from Safari

--- docs/.vuepress/components/Comments.vue
+++ docs/.vuepress/components/Comments.vue
@@ -1,14 +1,20 @@
 // Comments.vue
 <template>
-  <iframe
-    id="comentFrame"
-    v-bind:src="get_ifram_src_url"
-    frameborder="0"
-    height="800px"
-    width="100%"
-    >
-    コメントを表示するには iframe 機能を有効にして下さい
-  </iframe>
+  <div>
+    <iframe
+      id="comentFrame"
+      v-bind:src="get_ifram_src_url"
+      frameborder="0"
+      height="800px"
+      width="100%"
+      >
+      コメントを表示するには iframe 機能を有効にして下さい
+    </iframe>
+    <p>※コメント機能は外部サイトで構築しており、サードパーティCookieを利用しています。
+      Safariからコメントを投稿する場合、サードパーティCookieを有効化するか、もしくは
+      <a v-bind:href="get_ifram_src_url">コメントサイト</a> へ直接アクセスした上で
+      コメント投稿をお願いします。</p>
+  </div>
 </template>
 <script>
 export default {
